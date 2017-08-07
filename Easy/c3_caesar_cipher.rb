242f568779f9afe448af3efb49c2d5bb03ddd069
funwithcthulhu <thomasboeding@gmail.com>
2017-08-07 14:59:42 -0500

    5

--- Easy/c3_caesar_cipher.rb
+++ Easy/c3_caesar_cipher.rb
@@ -1,6 +1,6 @@
 def cipher(n)
-  n.to_s.split('').map {|q| q.ord}.map{|s| s == 122 ?
-  97 : s=s+1}.map {|z| z.chr }.join('')
+  n.to_s.split('').map(&:ord).map{|s| s == 122 ?
+  97 : s=s+1}.map(&:chr).join('')
 end
 
 def decoder(n)
