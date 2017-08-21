aa6a1c275052927f366667b44db1f461a77cc8db
funwithcthulhu <thomasboeding@gmail.com>
2017-08-21 16:21:16 -0500

    countdown prob

--- Easy/318_countdown.rb
+++ Easy/318_countdown.rb
@@ -1,21 +1,44 @@
-def countdown(*nums)
-  calculator = RPNCalculator.new
-  numbers = nums.to_a
-  target = numbers.pop
-  permutations = numbers.permutation(numbers.length).to_a.uniq!
-  operators = ['+', '-','*', '/']
-  permutations.each do |sub_array|
-      sub_array.map!(&:to_s)
-      sub_array.insert(1, operators.sample)
-      sub_array.insert(3, operators.sample)
-      sub_array.insert(5, operators.sample)
-      sub_array.insert(7, operators.sample)
-      sub_array.insert(9, operators.sample)
-      p sub_array.join('')
-      p calculator.evaluate(sub_array.join(''))
-      if calculator.evaluate(sub_array.join('')) == target 
-        print "#{sub_array.join(' ')} = #{target}"
-      end
+def countdown(string)
+  num_array = string.split(' ').map(&:to_i)
+  target = num_array.pop
+  size = num_array.size
+  operators = [:+, :-, :/, :*]
+  perm_array = num_array.permutation(size).to_a
+  op_array = operators.repeated_permutation(size-1).to_a
+  $str_array = []
+  ans = []
+  perms = perm_array.dup
+  while perms.length > 0
+    l = 0
+    p_temp = perms[l]
+    ops = op_array.dup
+    while ops.length > 0
+      i = 0
+      op_temp = ops[i]
+      $str_array.push(perms[l]+op_temp)
+      ops.delete(ops[i])
+      i += 1
+    end
+    perms.delete(perms[l])
+    l += 1
+  end
+  $str_array.map! { |array| array.join(' ') }
+  $str_array.each do |string|
+    puts string if RPNCalculator.new.evaluate(string) == target
+    ans.push(string) if RPNCalculator.new.evaluate(string) == target
+  end
+  working = []
+  ans.each do |string|
+    working.push(RPNCalculator.new.tokens(string))
+  end
+  final = []
+  working.each do |array|
+    a1 = array.select { |num| num.is_a? Integer}
+    a2 = array.select { |val| val.is_a? Symbol }
+    final.push(a1.zip(a2))
+  end
+  final.each do |arr|
+    puts "#{arr.flatten.compact.join(' ')} = #{target}"
   end
 end
 
@@ -25,54 +48,68 @@ class RPNCalculator
     @stack = stack
   end
 
-  def push(nums)
-    @stack.push nums
-  end
-  
   def plus
     if @stack.count < 2
       raise "calculator is empty"
     else
-      last = @stack.pop
-      second_to_last = @stack.pop
+      last = @stack.shift
+      second_to_last = @stack.shift
       added = last + second_to_last
-      @stack.push added
+      @stack.unshift added
     end
   end
-  
+
   def minus
     if @stack.count < 2
       raise "calculator is empty"
     else
-      last = @stack.pop
-      second_to_last = @stack.pop
-      subtracted = second_to_last - last
-      @stack.push subtracted
+      last = @stack.shift
+      second_to_last = @stack.shift
+      subtracted = last - second_to_last
+      @stack.unshift subtracted
     end
   end
-  
+
   def divide
     if @stack.count < 2
       raise "calculator is empty"
     else
-      last = @stack.pop
-      second_to_last = @stack.pop
-      divided = second_to_last.to_f / last.to_f
-      @stack.push divided
+      last = @stack.shift
+      second_to_last = @stack.shift
+      divided =  last.to_f / second_to_last.to_f
+      @stack.unshift divided
     end
   end
-  
+
   def times
     if @stack.count < 2
       raise "calculator is empty"
     else
-      last = @stack.pop
-      second_to_last = @stack.pop
-      multiplied = second_to_last.to_f * last.to_f
-      @stack.push multiplied
+      last = @stack.shift
+      second_to_last = @stack.shift
+      multiplied = last.to_f * second_to_last.to_f
+      @stack.unshift multiplied
     end
   end
-  
+
+  def tokens(string)
+    string.split(' ').each do |value|
+      case
+      when value == "*"
+        @stack.push(:*)
+      when value == "/"
+        @stack.push(:/)
+      when value == "+"
+        @stack.push(:+)
+      when value == "-"
+        @stack.push(:-)
+      else
+        @stack.push value.to_i
+      end
+    end
+    @stack
+  end
+
   def evaluate(string)
     @stack = []
     string.split(' ').each do |value|
@@ -91,10 +128,11 @@ class RPNCalculator
     end
     (string.include? '/') ? (@stack.join.to_f) : (@stack.join.to_i)
   end
-  
-  def value
-    @stack.last
-  end
 end
-  
-countdown(1, 3, 7, 6, 8, 3, 250)
+
+#example
+countdown('1 3 7 6 8 3 250')
+
+#challenges
+countdown('25 100 9 7 3 7 881')
+countdown('6 75 3 25 50 100 952')
--- Intermediate/312_next_largest_number.rb
+++ Intermediate/312_next_largest_number.rb
@@ -1,11 +1,9 @@
 def next_largest(num)
   final_array = []
   num_array = num.to_s.split('').map(&:to_i)
-  size = num_array.size
-  multi_array = num_array.permutation(size).to_a
-  multi_array = multi_array.each do |sub_array|
+  num_array.permutation(num_array.size).to_a.each do |sub_array|
     final_array << sub_array.map!(&:to_s).join('').to_i
   end
   final_array.delete_if { |val| val <= num }.sort!
-  return final_array[0]
+  final_array[0]
 end
