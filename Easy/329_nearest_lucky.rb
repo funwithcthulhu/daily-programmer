d0272396eed088c22db59ad2caa61acf97a4d77c
funwithcthulhu <thomasboeding@gmail.com>
2017-08-29 18:15:54 -0500

    329

--- Easy/329_nearest_lucky.rb
+++ Easy/329_nearest_lucky.rb
@@ -1,14 +1,36 @@
+require 'benchmark'
 
 def lucky(max)
-  start = Time.now
-  @array = 1.step(max, 2).to_a.unshift(0)
+  array = 1.step(max, 2).to_a.unshift(0)
   ind = 2
-  until @array[ind] == @array.last
-    val = @array[ind]
-    @array.each_index { |i| @array[i] = nil if i % val == 0 && @array[i] > val }
+  until array[ind] == array.last
+    val = array[ind]
+    array.each_index { |i| array[i] = nil if (i % val).zero? && array[i] > val }
     ind += 1
-    @array.compact!
+    array.compact!
   end
-  @array.last
-  puts Time.now - start
+  block_given? ? (yield array) : array
+end
+
+def nearest(num)
+  puts lucky(num).include?(num) ? "#{num} is Lucky" : find_near(num)
+end
+
+def find_near(num)
+  higher = num + 1
+  lower = num - 1
+  loop do
+    lucky(num * 2).include?(higher) ? break : higher += 1
+  end
+  loop do
+    lucky(num * 2).include?(lower) ? break : lower -= 1
+  end
+  "#{lower} < #{num} < #{higher}"
+end
+
+Benchmark.bm do |x|
+  x.report('lucky(5000): ') { lucky(5000) }
+  x.report('lucky(10_000): ') { lucky(10_000) }
+  x.report('lucky(1_000_000): ') { lucky(1_000_000) }
+  x.report('lucky(10_000_000): ') { lucky(10_000_000) }
 end
