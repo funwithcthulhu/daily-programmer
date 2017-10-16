aa6c6a534a1a3975bb0757f216515316fb99bef6
funwithcthulhu <thomasboeding@gmail.com>
2017-10-16 14:33:20 -0500

    336 easy

---
+++ Easy/336_cannibal_numbers.rb
@@ -0,0 +1,38 @@
+# Daily Programmer - Easy - #336
+# Example input:  7 2     
+#                 21 9 5 8 10 1 3
+#                 10 15 
+#
+# Output: 4 2
+
+def cannibalize(array, sz)
+  array = array.sort.reverse
+  cannibals = []
+  array.each { |v| cannibals << v if v >= sz }
+  cannibals.size.times { array.shift }
+  until array.empty?
+    candidate = array.shift
+    until array.empty? || candidate >= sz
+      array.pop
+      candidate += 1
+      cannibals << candidate if candidate >= sz
+    end
+  end
+  cannibals.size
+end
+
+def input_loop
+  puts 'press return on empty line to exit'
+  puts 'separate input with spaces: '
+  loop do
+    print 'numbers > '
+    input = gets.chomp.split(/\s+/).map(&:to_i)
+    break if input.empty?
+    print 'queries > '
+    queries = gets.chomp.split(/\s+/).map(&:to_i)
+    break if input.empty?
+    queries.each do |query|
+      puts "solution for query #{query}: #{cannibalize(input, query)}"
+    end
+  end
+end
